67dbecadbba8a43af21dbf9640ea7de94f0a680c
neyadrian <neyadrian2018@gmail.com>
2026-03-04 13:54:37 -0300

    Questão 01 - Uso de HashSet

---
+++ src/HashSet.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Set;
+
+public class HashSet {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Set<String> listaIps = new java.util.HashSet<>();
+        String ip;
+
+        System.out.println("--- Sistema de Segurança ---");
+        System.out.println("Digite 10 IPs:");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("IP %d: ", i + 1);
+            ip = sc.nextLine();
+
+            listaIps.add(ip);
+        }
+
+        System.out.println("\n--- Relatório Final ---");
+        System.out.println("Total de IPs únicos detectados: " + listaIps.size());
+
+        System.out.println("Lista de IPs que tentaram acesso:");
+        for (String ips: listaIps) {
+            System.out.println(ips);
+        }
+
+        sc.close();
+    }
+}
